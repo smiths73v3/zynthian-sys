d831aabf3c53598c83c1a32c70eee21af698bb00
smiths73v3 <smiths.73v3@gmail.com>
2025-07-17 21:02:55 -0400

    Update the recipe for DietPi possbily also raspbian?
    This method may need to be updated, the errors building on dietpi
    made it seem like this method was obsolete.

--- scripts/recipes/install_pyliblo.sh
+++ scripts/recipes/install_pyliblo.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-cd $ZYNTHIAN_SW_DIR
+#-#cd $ZYNTHIAN_SW_DIR
 
-if [ -d "pyliblo" ]; then
-	rm -rf pyliblo
-fi
+#-#if [ -d "pyliblo" ]; then
+#-#	rm -rf pyliblo
+#-#fi
 
-git clone https://github.com/dsacre/pyliblo.git
-cd pyliblo
-pip3 install .
-cd ..
+#-#git clone https://github.com/dsacre/pyliblo.git
+#-#cd pyliblo
+#-#pip3 install .
+#-#cd ..
 
 #--- Leave this around for now, so I can test the pyliblo install failure?
 #-#rm -rf pyliblo
+
+#Dietpi has a pyliblo package, so install that from apt, and get both the library and the python bindings
+apt-get -y install python3-pyliblo libpyliblo-dev
